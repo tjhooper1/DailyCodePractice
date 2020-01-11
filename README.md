e1cee369cc494939a5cca8176f9c10490a928ed8
tjhooper1 <tomh386@gmail.com>
2020-01-11 14:57:35 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#DAILY CODE PRACTICE  
+
+The purpose of this git is to hold a record of my daily code practices with Codewars.
+Each subdirectory will be titled what the code kata is named. Inside, there will be a code solutions folder and a readme that will describe what the problem is and some test cases. 
---
+++ sum-of-digits-digital-root/SumOfDigits.js
@@ -0,0 +1,20 @@
+function digital_root(n) {
+    const reducedNumber = calculate(n)
+    return reducedNumber;
+}
+
+function calculate(arr) {
+    const splitNumbers = arr
+        .toString()
+        .split("")
+        .map(number => parseInt(number));
+    let reduced = splitNumbers.reduce((acc, number) => {
+        return acc + number;
+    });
+    if (reduced > 9) {
+        reduced = calculate(reduced)
+    }
+    return reduced
+}
+
+console.log(digital_root(57))
