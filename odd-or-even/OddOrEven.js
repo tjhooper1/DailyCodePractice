31a847cdc29c076f01944618caac682cbdce869a
tjhooper1 <tomh386@gmail.com>
2020-01-22 22:51:04 -0500

    daily code challenge 1/22

--- odd-or-even/OddOrEven.js
+++ odd-or-even/OddOrEven.js
@@ -5,4 +5,6 @@ function oddOrEven(array) {
     return array.reduce((acc, cur) => acc + cur) % 2 === 0 ? "even" : "odd";
 }
 
-console.log(oddOrEven([2, 4, 6]));
+console.log(oddOrEven([2, 4, 6])); // 'even'
+console.log(oddOrEven([2, 4, 7])); // 'odd'
+console.log(oddOrEven([])); // 'even'
