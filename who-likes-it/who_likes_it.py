ce44e708cc42df2165e16a0e69b8590a49d6c175
thomas hooper <tomh386@gmail.com>
2020-06-08 22:16:01 -0400

    feat: 06/06/2020 who-likes-it

---
+++ who-likes-it/who_likes_it.py
@@ -0,0 +1,12 @@
+def likes(names):
+  if len(names) == 0:
+    return 'no one likes this'
+  if len(names) == 1:
+    return '{} likes this'.format(names[0])
+  if len(names) == 2:
+    return '{} and {} like this'.format(names[0], names[1])
+  if len(names) == 3:
+    return '{}, {} and {} like this'.format(names[0], names[1], names[2])
+  if len(names) > 3:
+    name_1,name_2, *rest = names
+    return '{}, {} and {} others like this'.format(name_1, name_2, len(rest))
